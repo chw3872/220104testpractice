54e26209d785022568e3ff51d20cdae56def3b0d
chw3872 <chw3872@gmail.com>
2022-01-17 22:45:55 +0900

    Q10164 백준 dp

---
+++ src/Q10164.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Q10164 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int point = Integer.parseInt(st.nextToken());
+
+        long[][] map = new long[n + 1][m + 1];
+        map[1][1] = 1;
+        if (point != 0) {
+            int pn = (point / m) + 1;
+            int pm = point % m;
+            for (int i = 1; i <= pn; i++) {
+                for (int j = 1; j <= pm; j++) {
+                    if (j - 1 > 0)
+                        map[i][j] += map[i][j - 1];
+                    if (i - 1 > 0)
+                        map[i][j] += map[i - 1][j];
+                }
+            }
+
+            for (int i = pn; i <= n; i++) {
+                for (int j = pm; j <= m; j++) {
+                    if (j - 1 >= pm)
+                        map[i][j] += map[i][j - 1];
+                    if (i - 1 >= pn)
+                        map[i][j] += map[i - 1][j];
+                }
+            }
+        } else {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= m; j++) {
+                    if (j - 1 > 0)
+                        map[i][j] += map[i][j - 1];
+                    if (i - 1 > 0)
+                        map[i][j] += map[i - 1][j];
+                }
+            }
+        }
+
+        System.out.println(map[n][m]);
+
+    }
+}
