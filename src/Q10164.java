1469f1a02f7a2208b828f92426e7249386078b1f
chw3872 <chw3872@gmail.com>
2022-01-18 16:21:26 +0900

    Q10164 백준 dp

--- src/Q10164.java
+++ src/Q10164.java
@@ -15,34 +15,36 @@ public class Q10164 {
 
         long[][] map = new long[n + 1][m + 1];
         map[1][1] = 1;
-        if (point != 0) {
-            int pn = (point / m) + 1;
-            int pm = point % m;
-            for (int i = 1; i <= pn; i++) {
-                for (int j = 1; j <= pm; j++) {
-                    if (j - 1 > 0)
-                        map[i][j] += map[i][j - 1];
-                    if (i - 1 > 0)
-                        map[i][j] += map[i - 1][j];
-                }
-            }
-
-            for (int i = pn; i <= n; i++) {
-                for (int j = pm; j <= m; j++) {
-                    if (j - 1 >= pm)
-                        map[i][j] += map[i][j - 1];
-                    if (i - 1 >= pn)
-                        map[i][j] += map[i - 1][j];
-                }
-            }
+        int pn = 0, pm = 0;
+        if (point == 0 || point == n * m) {
+            pn = n;
+            pm = m;
         } else {
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= m; j++) {
-                    if (j - 1 > 0)
-                        map[i][j] += map[i][j - 1];
-                    if (i - 1 > 0)
-                        map[i][j] += map[i - 1][j];
-                }
+            if (point % m == 0) {
+                pn = (point / m);
+                pm = m;
+            }
+            else {
+                pn = (point / m) + 1;
+                pm = point % m;
+            }
+        }
+
+        for (int i = 1; i <= pn; i++) {
+            for (int j = 1; j <= pm; j++) {
+                if (j - 1 > 0)
+                    map[i][j] += map[i][j - 1];
+                if (i - 1 > 0)
+                    map[i][j] += map[i - 1][j];
+            }
+        }
+
+        for (int i = pn; i <= n; i++) {
+            for (int j = pm; j <= m; j++) {
+                if (j - 1 >= pm)
+                    map[i][j] += map[i][j - 1];
+                if (i - 1 >= pn)
+                    map[i][j] += map[i - 1][j];
             }
         }
 
