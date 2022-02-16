a20f871e19ba2c22f709562293c121eeaa5fd26e
chw3872 <chw3872@gmail.com>
2022-02-16 20:10:42 +0900

    백준 220216

--- src/Q15927.java
+++ src/Q15927.java
@@ -7,11 +7,33 @@ public class Q15927 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         char[] chars = br.readLine().toCharArray();
-        char[] reverseChars = new char[chars.length];
 
-        for(int i = 0; i < chars.length; i++){
-            reverseChars[i] = chars[chars.length - i];
+        boolean palindrome = true;
+        for(int i = 0; i < chars.length / 2; i++){
+            if(chars[i] != chars[chars.length - i - 1]){
+                palindrome = false;
+            }
         }
 
+
+        if(palindrome)
+        {
+            char c = chars[0];
+            boolean isSame = true;
+            for(int i = 1; i < chars.length; i++)
+            {
+                if(c != chars[i]){
+                    isSame = false;
+                    break;
+                }
+            }
+            if(!isSame){
+                System.out.println(chars.length-1);
+            }
+            else
+                System.out.println(-1);
+        }else{
+            System.out.println(chars.length);
+        }
     }
 }
